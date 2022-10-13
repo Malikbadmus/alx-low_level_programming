05ec518ffbb9586917a0cbb8342e8ff5d378151b
malik badmus <malik.badmus@icloud.com>
2022-10-13 16:39:25 -0700

    sending

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main(void){
+/** 
+ *main- Serves as the starting point of the program
+ *
+ *Return: By default returns zero if no errors*/
+int main(void)
+{
   puts("\"Programming is like building a multilingual puzzle");
   
   return (0);
