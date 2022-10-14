44b746c13aeae426bd810341b2ea20ef8d8d4f33
malik badmus <malik.badmus@icloud.com>
2022-10-14 15:27:54 -0700

    correction

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main-program starting point
+*Return:0 if succesfull
+*/
+int main(void)  
+{
+char i;
+char j;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
