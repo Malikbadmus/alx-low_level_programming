9a57e5682cd9128d0a044984cce800abba4894c1
malik badmus <malik.badmus@icloud.com>
2022-10-14 17:11:45 -0700

    correction

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main-program starting point
+*Return: 0 if sucessfull
+*/
+int main(void) 
+{
+char i;
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
