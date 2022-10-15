f9432b0fe6d26cf149fa44963a578cd7dded397e
malik badmus <malik.badmus@icloud.com>
2022-10-14 17:37:48 -0700

    correction

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,10 +6,10 @@
 int main(void) 
 {
 int i;
- char hex[] = "0123456789abcdef";
+char hex[] = "0123456789abcdef";
 for (i = 0; i < 16; i++)
 {
-putchar(hex[I]);
+putchar(hex[i]);
 }
 putchar('\n');
 return (0);
