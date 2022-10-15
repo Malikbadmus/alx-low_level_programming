5bcc6582556004b0050885d0c258d0958392100d
malik badmus <malik.badmus@icloud.com>
2022-10-14 17:28:42 -0700

    correction

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main-program starting point
+*Return: 0 if sucessfull
+*/
+int main(void)
+{
+char i;
+for (i = 'z'; i >= 'a'; i--)
+putchar(i);
+putchar('\n');  
+return (0);
+}
