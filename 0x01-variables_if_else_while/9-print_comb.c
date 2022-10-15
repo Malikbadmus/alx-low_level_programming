bf2d606f12237440b599095963a0a6b1d417cd27
malik badmus <malik.badmus@icloud.com>
2022-10-14 17:46:28 -0700

    correction

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,7 +8,7 @@ int main(void)
 int num;
 for (num = '0'; num <= '9'; num++)
 {
-putchar(num)
+putchar(num);
 if (num == '9')
 {
 break;
@@ -19,6 +19,6 @@ putchar(',');
 putchar(' ');
 }
 }
-putchar('\n') 
+putchar('\n'); 
 return (0); 
 }
