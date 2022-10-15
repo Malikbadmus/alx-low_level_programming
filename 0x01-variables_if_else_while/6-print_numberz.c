1dc98449b232c0651620f5663eee9488492785c7
malik badmus <malik.badmus@icloud.com>
2022-10-14 17:21:01 -0700

    correction

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,11 +3,11 @@
 *main-program starting point
 *Return: 0 if sucessfull
 */
-int main(void) 
-{  
+int main(void)
+{
 int c;
 for (c= '0'; c <= '9'; c++)
 putchar(c);
-putchar('\n');  
+putchar('\n');
 return (0);
 }
