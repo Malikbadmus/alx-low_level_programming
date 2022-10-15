ed2be1c0a3c2d55d88999380abd974093ba04cb8
malik badmus <malik.badmus@icloud.com>
2022-10-14 17:17:25 -0700

    correction

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main-program starting point
+*Return: 0 if sucessfull
+*/
+int main(void) 
+{  
+int c;
+for (c= '0'; c <= '9'; c++)
+putchar(c);
+putchar("\n");  
+return (0);
+}
