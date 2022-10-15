41576b423def70474277abc566c23ca90afe87e6
malik badmus <malik.badmus@icloud.com>
2022-10-14 17:34:27 -0700

    correction

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main-program starting point
+*Return: 0 if sucessfull
+*/
+int main(void) 
+{
+int i;
+char hex[] = "0123456789abcdef"
+for (i = 0; i < 16; i++)
+{
+putchar(hex[I]);
+}
+putchar('\n');
+return (0);
+}
