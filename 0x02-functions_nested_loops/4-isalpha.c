1df3d6dd881e877135f5d7b5f5925ee3fdc39228
malik badmus <malik.badmus@icloud.com>
2022-10-18 15:55:12 -0700

    correction

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks if a character is alphabetic.
+ * @calph: The character to be checked.
+ *
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
+ */
+
+int _isalpha(int calph)
+{
+	if (calph >= 'a' && calph <= 'z') ||
+		(calph >= 'A' && calph <= 'Z')
+			return (1);
+	else
+		return (0);
+}
