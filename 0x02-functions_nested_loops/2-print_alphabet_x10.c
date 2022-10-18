d78ba499432183b3779a3337a27f7b5d5f8f7b83
malik badmus <malik.badmus@icloud.com>
2022-10-18 15:19:29 -0700

    correction

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10
+ */
+
+void print_alphabet_x10()
+{
+	for (j = 1; j <= 10; ++j){
+		for (alph = 'a'; alph < 'z'; ++alph)
+			_putchar(alph);
+		_putchar(alph);
+	}
+}
