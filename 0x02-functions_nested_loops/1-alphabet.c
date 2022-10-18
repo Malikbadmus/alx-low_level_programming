32b506a7d12e04bea19e6dc36d84293e4dc0eb00
malik badmus <malik.badmus@icloud.com>
2022-10-18 13:03:03 -0700

    correction

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main-Program starting point
+ *Return: 0 if successful
+ */
+
+void alphabet()
+{
+	char alph;
+	for(alph = 'a'; alph < 'z'; ++alph)
+		putchar(alph);
+}
+int main(void)
+{
+	alphabet();
+	return (0);
+}
