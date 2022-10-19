2f0e9f54ca7c8e625a601cb5c22a0de35cba91a2
malik badmus <malik.badmus@icloud.com>
2022-10-18 18:54:44 -0700

    correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,35 +10,33 @@ void times_table(void)
 
 {
 
-	int num, mult, prod;
+int num, mult, prod;
 
-
-
-	for (num = 0; num <= 9; num++)
+for (num = 0; num <= 9; num++)
 
 {
 
-		_putchar('0');
-    	for (mult = 1; mult <= 9; mult++)
+	_putchar('0');
 
+    	for (mult = 1; mult <= 9; mult++)
 	{
 
-	_putchar(',');
+		_putchar(',');
 
 		_putchar(' ');
 
-	prod = num * mult;
+		prod = num * mult;
 
 
 
-	if (prod <= 9)
+		if (prod <= 9)
 
-		_putchar(' ');
+			_putchar(' ');
+		else
 
-	else
-		_putchar((prod / 10) + '0');
+			_putchar((prod / 10) + '0');
 
-       	_putchar((prod % 10) + '0');
+		_putchar((prod % 10) + '0');
 
 }
 	_putchar('\n');
