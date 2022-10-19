45661b3542085601a5e5769f10a5425c28e2c0f1
malik badmus <malik.badmus@icloud.com>
2022-10-18 20:17:24 -0700

    correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -47,17 +47,16 @@ if (n >= 0 && n <= 15)
 
 
 			if (prod >= 100)
-		{\n
-				
+			{
+
 				_putchar((prod / 100) + '0');
 
 				_putchar(((prod / 10)) % 10 + '0');
 
-				}
 
 			else if (prod <= 99 && prod >= 10)
 
-			{
+
 
 				_putchar((prod / 10) + '0');
 
