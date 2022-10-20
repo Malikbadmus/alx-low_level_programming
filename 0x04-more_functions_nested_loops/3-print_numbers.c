bb15c0993a7cb41ecd5d1aee8fb2246891f606f5
malik badmus <malik.badmus@icloud.com>
2022-10-20 13:38:52 -0700

    correction

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  *print_numbers- it print numbers
- *@i: variable
  *Return: 0 if successful
  */
 
@@ -12,6 +11,5 @@ void print_numbers(void)
 	for (i = 0; i <= 10; i++)
 
 		_putchar(i + '0');
-
 	_putchar('\n');
 }
