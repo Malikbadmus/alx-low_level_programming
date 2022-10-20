87d9f40fb550f07154e53bb858f940fcefa99c64
malik badmus <malik.badmus@icloud.com>
2022-10-20 14:05:59 -0700

    correction

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,37 @@
+#include <main.h>
+/**
+ *-more_numbers- starting point
+ *Return: 0 if sucessfull
+ */
+
+void more_numbers(void)
+{
+	int i, y;
+
+
+
+	for (y = 0; y < 10; y++)
+
+	{
+
+		for (i = 0; i <= 14; i++)
+
+		{
+
+			if (i >= 10)
+
+			{
+
+				_putchar((i / 10) + '0');
+
+			}
+
+			_putchar((i % 10) + '0');
+
+		}
+
+		_putchar('\n');
+
+	}
+
+}
