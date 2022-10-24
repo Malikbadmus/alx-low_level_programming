6878f22808d519ac4387d0c39a23a6d5d3ff7e76
malik badmus <malik.badmus@icloud.com>
2022-10-24 16:27:09 -0700

    correction

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,8 +3,8 @@
 
 
 /**
- * puts2 - prints every other character of a  string to stdout
- * @str: string to print
+ * puts2 - Prints every other character of a string
+ * @str: The string to be treated
  *
  * Return: void
  */
@@ -12,19 +12,31 @@ void puts2(char *str)
 
 {
 
-	int i = 0;
+	int i;
+
+	int j = 0;
 
 
 
-	while (*(str + i))
+	while (str[j] != '\0')
 
 	{
 
-		_putchar(*(str + i));
-
-		i = i + 2;
+		j++;
 
 	}
+
+
+
+	for (i = 0; i < j; i += 2)
+
+	{
+
+		_putchar(str[i]);
+
+	}
+
+
 
 	_putchar('\n');
 
