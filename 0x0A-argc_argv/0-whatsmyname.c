2a4191d0cab15c41302046a4d1c27bf673adda81
malik badmus <malik.badmus@icloud.com>
2022-11-04 14:25:14 -0700

    correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,8 @@
 
 int main(int argc, char *argv[])
 {
-	void (argc);
 	
-	_putchar("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-int _putchar(char c);
 int main(int argc, char *argv[]);
 
 #endif
