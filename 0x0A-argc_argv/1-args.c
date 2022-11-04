add0e0fc45e6f7b09f6bd09f98e0527b23c69d16
malik badmus <malik.badmus@icloud.com>
2022-11-04 14:42:53 -0700

    correction

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main- program starting point
+ *@argc: Variable
+ *@argv: variable
+ *Return: 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+		printf("%s", argv[i]);
+       
+
+	return (0);
+}
